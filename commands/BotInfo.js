187fd4a7843eb4b3d291f608e738201e130a25ba
Matin Massoudi <matin@massoudi.net>
2022-05-08 14:15:56 -0700

    Stats command fixes

--- commands/BotInfo.js
+++ commands/BotInfo.js
@@ -1,23 +1,48 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { Client, MessageEmbed } = require('discord.js');
+var process = require('process')
+var osu = require('node-os-utils')
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('savvystatus')
+		.setName('status')
 		.setDescription('returns savvys ping'),
 	async execute(client, interaction) {
+		
+		const memoryUsed = process.memoryUsage().heapUsed;
+		var cpu = osu.cpu;
+		var mem = osu.mem;
+		var cpuUtil = await cpu.usage();
+		var memUtil = await mem.info();
+		
 		const replyEmbed = new MessageEmbed()
 			.setColor('#0099ff')
-			.setTitle(`Savvy's current status:`)
+			.setTitle(`Savvy's current status`)
 			.addFields(
-				{ name: 'Uptime', value: `\t**${new Date(client.uptime).getHours()}**`, inline: true },
-				{ name: '\tLast READY state', value: `**${client.readyAt.toLocaleDateString("en-US")}**`, inline: true },
-				{ name: `\tCreated on`, value: `\t**${client.application.createdAt.toLocaleDateString("en-US")}**`, inline: true },
-				{ name: `Created by`, value: `**${"Matin M"}**`, inline: true },
-				{ name: `\tNumber of channels`, value: `\t**${client.channels.cache.size}**`, inline: true },
-				{ name: `Number of guilds`, value: `**${client.guilds.cache.size}**`, inline: true },
+				{ name: 'Uptime', value: `**${msToTime(client.uptime)}**`, inline: true },
+				{ name: 'Number of guilds', value: `**${client.guilds.cache.size}**`, inline: true },
+				{ name: `Created on`, value: `**${client.application.createdAt.toLocaleDateString("en-US")}**`, inline: true },
+				{ name: `Heap usage`, value: `**${Math.round(memoryUsed * 100) / 100} bytes**`, inline: true },
+				{ name: `Free mem`, value: `**${memUtil.freeMemPercentage}%**`, inline: true },
+				{ name: `CPU usage`, value: `**${cpuUtil}%**`, inline: true },
+				{ name: `Processor type`, value: `**${cpu.model()}**`, inline: true },
+				{ name: `Processor core count`, value: `**${cpu.count()}**`, inline: true },
 			)
 			.setTimestamp()
+		
 		await interaction.reply({embeds: [replyEmbed]});
 	},
 };
+
+function msToTime(duration) {
+  var milliseconds = Math.floor((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
