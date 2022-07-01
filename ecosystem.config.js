3b7357e3ac621ec74cec3371a1be7667a37d683b
Matin Massoudi <matin@massoudi.net>
2022-07-01 15:46:37 -0700

    Upgraded deps

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,15 @@ module.exports = {
   apps: [
     {
       name: "Savvy",
-      script: "./src/index.js",
+      cwd: "./src/",
+      script: "./index.js",
+      args: "",
+      watch: false,
+      node_args: "",
       exec_mode: "fork",
       log_date_format: "YYYY-MM-DD HH:mm Z",
-      error_file: "./ProcessLogs/stderr-logs.txt",
-      out_file: "./ProcessLogs/stdout-logs.txt",
+      error_file: "../ProcessLogs/stderr-logs.txt",
+      out_file: "../ProcessLogs/stdout-logs.txt",
       autorestart: true,
     },
   ],
