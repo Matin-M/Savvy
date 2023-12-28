8aef00514043ae935bbf8f477796ba9c3b092c9a
Matin-M <matin@massoudi.net>
2023-12-27 20:27:01 -0800

    Prune old stats

--- src/commands/ServerInfo.ts
+++ src/commands/ServerInfo.ts
@@ -6,7 +6,6 @@ import {
 } from 'discord.js';
 import { Model, ModelCtor } from 'sequelize/types';
 import { CustomClient } from '../types/CustomClient';
-import { keySort, wordFreq } from '../helpers/utils';
 
 export default {
   data: new SlashCommandBuilder()
@@ -17,28 +16,6 @@ export default {
     interaction: ChatInputCommandInteraction<CacheType>,
     Tags: ModelCtor<Model<any, any>>
   ) {
-    const tag = (await Tags.findOne({
-      where: { guildId: interaction.guild!.id },
-    }))!;
-    const messages = [
-      ...new Set(tag.get('user_message_logs') as [Record<string, any>]),
-    ];
-    const deleted_messages = [
-      ...new Set(tag.get('deleted_user_message_logs') as string[]),
-    ];
-    const freq = wordFreq(
-      messages.map((msg) => msg.userMessage as string) as [string]
-    );
-    const freqTable = keySort(freq, (item) => item);
-
-    const members = await interaction.guild!.members.fetch();
-    const userFreq = wordFreq(
-      messages.map((msg) => msg.userID as string) as [string]
-    );
-    const userFreqTable = keySort(userFreq, (userID) =>
-      members.find((member) => member.id === userID)
-    );
-
     const replyEmbed = new EmbedBuilder()
       .setColor('#0099ff')
       .setTitle(`Server info for ${interaction.guild!.name}`)
@@ -61,28 +38,8 @@ export default {
           inline: true,
         },
         {
-          name: `Messages sent`,
-          value: `**${messages.length}**`,
-          inline: true,
-        },
-        {
-          name: `Messages deleted`,
-          value: `\t**${deleted_messages.length}**`,
-          inline: true,
-        },
-        {
           name: `Server ID`,
           value: `**${interaction.guild!.id}**`,
-          inline: true,
-        },
-        {
-          name: `Word frequency`,
-          value: `**${freqTable}**`,
-          inline: true,
-        },
-        {
-          name: `Most active chatters`,
-          value: `**${userFreqTable}**`,
           inline: true,
         }
       )
