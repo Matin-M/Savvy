cb154380c8caf73bac99049dd90622395ea1f56a
Matin Massoudi <matin@massoudi.net>
2024-03-26 10:47:35 -0700

    Refactored gamestats

--- src/commands/GetGameStats.ts
+++ src/commands/GetGameStats.ts
@@ -4,11 +4,9 @@ import {
   ChatInputCommandInteraction,
   EmbedBuilder,
 } from 'discord.js';
+import { makeFortniteAPIRequest } from '../helpers/utils';
 import { Model, ModelCtor } from 'sequelize/types';
-import { fortnite_api_key } from '../config.json';
 import { CustomClient } from '../types/CustomClient';
-import { ServerResponse } from '../types/APIInterfaces';
-import axios, { AxiosResponse } from 'axios';
 
 export default {
   data: new SlashCommandBuilder()
@@ -18,7 +16,7 @@ export default {
       option
         .setName('username')
         .setDescription('Epic games username')
-        .setRequired(true)
+        .setRequired(false)
     )
     .addBooleanOption((option) =>
       option
@@ -29,91 +27,102 @@ export default {
   async execute(
     client: CustomClient,
     interaction: ChatInputCommandInteraction<CacheType>,
-    Tags: ModelCtor<Model<any, any>>
+    Tags: ModelCtor<Model<any, any>>,
+    PresenceTable: ModelCtor<Model<any, any>>,
+    ClientMessageLogs: ModelCtor<Model<any, any>>,
+    PreferenceTable: ModelCtor<Model<any, any>>
   ) {
     const replyEmbed = new EmbedBuilder();
-    const username = interaction.options.getString('username');
-    const timespan = interaction.options.getBoolean('lifetime');
-    await axios
-      .get<ServerResponse>(
-        `https://fortnite-api.com/v2/stats/br/v2?name=${username}&accountType=epic&timeWindow=${
-          timespan ? 'lifetime' : 'season'
-        }&image=all`,
+    let username = interaction.options.getString('username');
+    const timespan = interaction.options.getBoolean('lifetime')
+      ? 'lifetime'
+      : 'season';
+
+    if (!username) {
+      const storedUsername = await PreferenceTable.findOne({
+        where: { guildId: interaction.guild!.id, key: 'fortniteUsername' },
+      });
+      if (!storedUsername || !storedUsername.get('value')) {
+        replyEmbed
+          .setColor([255, 0, 0])
+          .setTitle('Error!')
+          .setDescription('No username provided and no stored username found.');
+        await interaction.reply({ embeds: [replyEmbed], ephemeral: true });
+        return;
+      }
+      username = storedUsername.get('value') as string;
+    } else {
+      await PreferenceTable.upsert({
+        guildId: interaction.guild!.id,
+        key: 'fortniteUsername',
+        value: username,
+      });
+    }
+
+    const response = await makeFortniteAPIRequest(username, timespan);
+    if (!response) {
+      replyEmbed
+        .setColor([255, 0, 0])
+        .setTitle('Error!')
+        .setDescription('A user with the specified username was not found');
+      await interaction.reply({ embeds: [replyEmbed], ephemeral: true });
+      return;
+    }
+    const data = response.data;
+    replyEmbed
+      .setTitle(
+        `${data.account.name}'s ${timespan ? 'Lifetime' : 'Season'} Stats`
+      )
+      .addFields(
         {
-          headers: { Authorization: fortnite_api_key },
+          name: 'Playtime',
+          value: `${(data.stats.all.overall.minutesPlayed / 60).toFixed(
+            2
+          )} Hours`,
+          inline: true,
+        },
+        {
+          name: 'Battlepass Level',
+          value: `${data.battlePass.level}`,
+          inline: true,
+        },
+        {
+          name: 'Players Outlived',
+          value: `${data.stats.all.overall.playersOutlived}`,
+          inline: true,
+        },
+        {
+          name: 'Score',
+          value: `${data.stats.all.overall.score}`,
+          inline: true,
+        },
+        {
+          name: 'Score/min',
+          value: `${data.stats.all.overall.scorePerMin}`,
+          inline: true,
+        },
+        {
+          name: 'Score/match',
+          value: `${data.stats.all.overall.scorePerMatch}`,
+          inline: true,
+        },
+        {
+          name: 'Top 10',
+          value: `${data.stats.all.overall.top10} finishes`,
+          inline: true,
+        },
+        {
+          name: 'Top 5',
+          value: `${data.stats.all.overall.top5} finishes`,
+          inline: true,
+        },
+        {
+          name: 'Top 3',
+          value: `${data.stats.all.overall.top3} finishes`,
+          inline: true,
         }
       )
-      .catch((err) => {
-        console.error(err);
-      })
-      .then((res: void | AxiosResponse<ServerResponse, any>) => {
-        if (!res) {
-          replyEmbed
-            .setColor([255, 0, 0])
-            .setTitle('Error!')
-            .setDescription('A user with the specified username was not found');
-          interaction.reply({ embeds: [replyEmbed], ephemeral: true });
-          return;
-        }
-        const { data } = res;
-        // lol
-        const overallStats = data.data.stats.all.overall;
-        replyEmbed
-          .setTitle(
-            `${data.data.account.name}'s ${
-              timespan ? 'lifetime' : 'season'
-            } Stats`
-          )
-          .addFields(
-            {
-              name: 'Playtime',
-              value: `${(overallStats.minutesPlayed / 60).toFixed(2)} Hours`,
-              inline: true,
-            },
-            {
-              name: 'Battlepass Level',
-              value: `${data.data.battlePass.level}`,
-              inline: true,
-            },
-            {
-              name: 'Players Outlived',
-              value: `${overallStats.playersOutlived}`,
-              inline: true,
-            },
-            {
-              name: 'Score',
-              value: `${overallStats.score}`,
-              inline: true,
-            },
-            {
-              name: 'Score/min',
-              value: `${overallStats.scorePerMin}`,
-              inline: true,
-            },
-            {
-              name: 'Score/match',
-              value: `${overallStats.scorePerMatch}`,
-              inline: true,
-            },
-            {
-              name: 'Top 10',
-              value: `${overallStats.top10} finishes`,
-              inline: true,
-            },
-            {
-              name: 'Top 5',
-              value: `${overallStats.top5} finishes`,
-              inline: true,
-            },
-            {
-              name: 'Top 3',
-              value: `${overallStats.top3} finishes`,
-              inline: true,
-            }
-          )
-          .setImage(data.data.image);
-        interaction.reply({ embeds: [replyEmbed] });
-        return;
-      });
+      .setImage(data.image);
+    await interaction.reply({ embeds: [replyEmbed] });
   },
 };
